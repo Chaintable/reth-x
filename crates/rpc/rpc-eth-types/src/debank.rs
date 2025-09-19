7b237567535e10d4903809d38d6f204ca5ecb1de
0byteme <819643718@qq.com>
2025-09-19 17:21:51 +0800

    fix newCodes bytes

--- crates/rpc/rpc-eth-types/src/debank.rs
+++ crates/rpc/rpc-eth-types/src/debank.rs
@@ -99,7 +99,7 @@ pub fn get_storage_diffs_from_cache<DB: DatabaseRef>(cache: Cache, pre_db: DB) -
                     continue; // Code already exists in the previous state
                 }
             }
-            new_codes.push(NewCode { code_hash, code: code.bytes() });
+            new_codes.push(NewCode { code_hash, code: code.original_bytes() });
         }
     }
 
